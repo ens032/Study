4612bf8878699bba7eada0031a4d06ad05c68374
Enes Efe Ş. <enesefesesenoglu@gmail.com>
2024-08-11 14:12:31 +0300

    uploadfiles

---
+++ study.py
@@ -0,0 +1,26 @@
+#practice_list=[[10,20,30],[40,50,60],[70,80,90]]
+#for nested_list in practice_list:
+#    for value in nested_list:
+#     if value == 50 :
+#        continue
+#    print(value)
+
+#practice_list=[[10,20,30],[40,50,60],[70,80,90]]
+#for nested_list in practice_list:
+#   for value in nested_list:
+#      if value < 50:
+#         print("başlangıç")
+#      if value == 50:
+#         print("Tam Ortası")
+#      if value > 50:
+#         break
+#      print(value)
+
+#def calculator(num1,num2,operator):
+#    if operator == "minus":
+#        result=num1-num2
+#        print(f"{num1}-{num2}={result}")
+#    if operator == "plus":
+#        result=num1+num2
+#        print(f"{num1}+{num2}={result}")
+#calculator(50,70,"plus")
